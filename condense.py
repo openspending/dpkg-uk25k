d13c2515deff236a65789eefb6f6c3867cda275f
Andrew Suffield <asuffield@suffields.me.uk>
2012-02-14 09:01:39 +0000

    Get condense and formats working

--- condense.py
+++ condense.py
@@ -1,23 +1,38 @@
 from functools import partial
 
 import sqlaload as sl
+import logging
+import sys
+import time
 
 from common import *
 
-def condense(engine, raw_table_name, resource_id, table_id):
+def condense(engine, resource_id, table_id, force):
+    table_suffix = '%s_table%s' % (resource_id, table_id)
+
+    if not engine.has_table('raw_%s' % table_suffix):
+        return
+
+    condensed_table = sl.get_table(engine, 'condensed')
+
+    # Skip over tables we have already extracted
+    if not force and sl.find_one(engine, condensed_table, resource_id=resource_id, table_id=table_id) is not None:
+        return
+
     connection = engine.connect()
     trans = connection.begin()
 
+    start = time.time()
+
     try:
-        raw_table = sl.get_table(connection, raw_table_name)
-        spending_table = sl.get_table(connection, 'spending')
+        raw_table = sl.get_table(connection, 'raw_%s' % table_suffix)
+        sl.drop_table(connection, 'spending_%s' % table_suffix)
+        spending_table = sl.get_table(connection, 'spending_%s' % table_suffix)
         columns_table = sl.get_table(connection, 'columns')
         mappings = dict([(c.get('original'), c.get('column')) for c in \
                          sl.all(connection, columns_table) if c.get('valid')])
         for row in sl.all(connection, raw_table):
-            spending_row = {'resource_id': resource_id,
-                            'table_id': table_id,
-                            'row_id': row['id'],}
+            spending_row = {}
             for key, value in row.items():
                 if key not in mappings:
                     continue
@@ -27,22 +42,30 @@ def condense(engine, raw_table_name, resource_id, table_id):
                     continue
                 spending_row[mappings[key]] = value
             #print spending_row
-            sl.upsert(connection, spending_table, spending_row, 
-                      ['resource_id', 'table_id', 'row_id'])
+            sl.add_row(connection, spending_table, spending_row)
+        sl.upsert(connection, condensed_table, {'resource_id': resource_id,
+                                                'table_id': table_id,
+                                                'condense_time': time.time() - start,
+                                                }, ['resource_id', 'table_id'])
         trans.commit()
     finally:
         connection.close()
 
-def describe(raw_table_name):
-    return 'condense: %s' % raw_table_name
+def describe(resource_id, table_id):
+    return 'condense: %s %s' % (resource_id, table_id)
 
 def test_condense_all():
     engine = db_connect()
     table = sl.get_table(engine, 'extracted')
     for row in sl.all(engine, table):
         for table_id in xrange(0, row['max_table_id'] + 1):
-            raw_table_name = 'raw_%s_table%s' % (row['resource_id'], table_id)
-            if engine.has_table(raw_table_name):
-                condense_ = partial(condense, engine, raw_table_name, row['resource_id'], table_id)
-                condense_.description = describe(raw_table_name)
-                yield condense_
+            condense_ = partial(condense, engine, row['resource_id'], table_id, False)
+            condense_.description = describe(row['resource_id'], table_id)
+            yield condense_
+
+if __name__ == '__main__':
+    logging.basicConfig()
+    logging.getLogger(__name__).setLevel(logging.DEBUG)
+    logging.getLogger('sqlalchemy.engine').setLevel(logging.WARN)
+    engine = db_connect()
+    condense(engine, sys.argv[1], sys.argv[2], True)
--- formats.py
+++ formats.py
@@ -1,13 +1,29 @@
 from collections import defaultdict
 from functools import partial
 from datetime import datetime
+from xlrd.xldate import xldate_as_tuple
 
 import sqlaload as sl
+import logging
+import sys
+import time
+
+from common import *
 
 NUMERIC_FIELDS = ['Amount', 'VATNumber']
 DATE_FIELDS = ['Date']
 
-FORMATS = ['%Y-%m-%d', '%Y-%m-%d %H:%M:%S', '%d/%m/%Y', '%m/%d/%Y']
+FORMATS = [
+    # Variations on sensible date formats
+    '%Y-%m-%d', '%Y-%m-%d %H:%M:%S.%f', '%Y-%m-%d %H:%M:%S',
+    # Less sensible date formats
+    '%d/%m/%Y', '%d/%m/%Y %H:%M', '%m/%d/%Y', '%d/%m/%y', '%d.%m.%y', '%d.%m.%Y',
+    # Ridiculous ones
+    '%m/%d/%y', '%Y%m%d',
+    # Things with words in
+    '%d-%b-%y', '%d-%b-%Y', '%d/%b/%Y', '%b-%y', '%d %B %Y', '%d %b %y',
+    # Things we'd prefer to forget
+    'excel']
 
 def detect_date_format(values):
     # TODO: alternative solution - some sheets use more than one date format, 
@@ -20,9 +36,15 @@ def detect_date_format(values):
 
     scores = defaultdict(int)
     for (value, weight) in values_.items():
+        if value is None:
+            continue
         for format_ in FORMATS:
             try:
-                datetime.strptime(value, format_)
+                if format_ == 'excel':
+                    # Since it's the only integer format supported, this isn't bad. It's 1982 .. 2036 ish
+                    assert float(value) > 30000 and float(value) < 50000
+                else:
+                    datetime.strptime(value.strip(), format_)
                 scores[format_] += weight
             except: pass
     scores = sorted(scores.items(), key=lambda (f,n): n)
@@ -31,12 +53,13 @@ def detect_date_format(values):
     return scores[-1][0]
 
 
-def format(engine, table, section):
+def do_format(engine, resource_id, table_id):
+    table_suffix = '%s_table%s' % (resource_id, table_id)
+
+    table = sl.get_table(engine, 'spending_%s' % table_suffix)
 
     date_field_values = defaultdict(list)
-    for row in sl.find(engine, table,
-        resource_id=section.get('resource_id'),
-        table_id=section.get('table_id')):
+    for row in sl.all(engine, table):
         for date_field in DATE_FIELDS:
             if date_field in row and row[date_field]:
                 date_field_values[date_field].append(row[date_field])
@@ -45,9 +68,7 @@ def format(engine, table, section):
     for date_field, values in date_field_values.items():
         date_field_formats[date_field] = detect_date_format(values)
 
-    for row in sl.find(engine, table,
-        resource_id=section.get('resource_id'),
-        table_id=section.get('table_id')):
+    for row in sl.all(engine, table):
 
         for numeric_field in NUMERIC_FIELDS:
             try:
@@ -65,26 +86,54 @@ def format(engine, table, section):
             try:
                 if row[date_field] is None:
                     continue
-                parsed = datetime.strptime(row[date_field], format_)
+                if format_ == 'excel':
+                    # Deciphers excel dates that have been mangled into integers by formatting errors
+                    parsed = datetime(*xldate_as_tuple(float(row[date_field].strip()), 0))
+                else:
+                    parsed = datetime.strptime(row[date_field].strip(), format_)
                 row[date_field + 'AsDate'] = parsed.strftime("%Y-%m-%d")
             except Exception as e:
                 print e
 
-        sl.upsert(engine, table, row, 
-                  ['resource_id', 'table_id', 'row_id'])
+        sl.upsert(engine, table, row, ['id'])
+
+def format(engine, resource_id, table_id, force):
+    connection = engine.connect()
+    trans = connection.begin()
+    condensed_table = sl.get_table(connection, 'condensed')
+    condensed_row = sl.find_one(connection, condensed_table, resource_id=resource_id, table_id=table_id)
+    if condensed_row is None:
+        condensed_row = {'resource_id': resource_id,
+                         'table_id': table_id,
+                         }
+    start = time.time()
+    try:
+        if not force and condensed_row['format_time'] is not None:
+            return
+        do_format(connection, resource_id, table_id)
+        condensed_row['format_time'] = time.time() - start
+        sl.upsert(connection, condensed_table, condensed_row, ['resource_id', 'table_id'])
+        trans.commit()
+    finally:
+        connection.close()
+
+def connect():
+    engine = db_connect()
+    table = sl.get_table(engine, 'spending')
+    return engine,table
 
 def test_format_all():
-    engine = sl.connect("sqlite:///uk25k.db")
-    table = sl.get_table(engine, 'spending')
-    for row in sl.distinct(engine, table, 'resource_id', 'table_id'):
-        format_ = partial(format, engine, table, row)
+    engine,table = connect()
+    table = sl.get_table(engine, 'condensed')
+    for row in sl.all(engine, table):
+        format_ = partial(format, engine, row['resource_id'], row['table_id'], False)
         format_.description = \
             'format: %(resource_id)s/%(table_id)s' % row
         yield format_
 
-
-
-
-
-
-
+if __name__ == '__main__':
+    logging.basicConfig()
+    logging.getLogger(__name__).setLevel(logging.DEBUG)
+    logging.getLogger('sqlalchemy.engine').setLevel(logging.WARN)
+    engine,table = connect()
+    format(engine, sys.argv[1], sys.argv[2], True)

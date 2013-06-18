deb2c7b10e3a40086f944cd3ef342514e4d301a6
Friedrich Lindenberg <friedrich@pudo.org>
2013-06-18 07:59:59 -0400

    Make script tolerate errors.

--- datawire.py
+++ datawire.py
@@ -1,4 +1,5 @@
 import sqlaload as sl
+import time
 from datastringer import DataStringer
 
 from common import *
@@ -13,13 +14,21 @@ def submit_all():
     stringer = DataStringer(service='ukspending',
                             event='transactions')
     log.info("Submitting frames to datawire...")
+    failures = 0
     for i, row in enumerate(generate_all()):
         action_at = row.get('DateFormatted')
         if action_at:
             action_at = datetime.datetime.strptime(action_at, '%Y-%m-%d')
-        stringer.submit(row,
-                        action_at=action_at,
-                        source_url=row.get('SourceURL'))
+        try:
+            stringer.submit(row,
+                            action_at=action_at,
+                            source_url=row.get('SourceURL'))
+            failures = 0
+        except Exception, e:
+            failures += 1
+            if failures > 10:
+                raise e
+            time.sleep(5)
     data = {'datawire_submitted': True}
     sl.update(engine, spending, row)
 

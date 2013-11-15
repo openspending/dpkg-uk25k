b9f788abf34ce57e840a5df12ae4a7d58c0e03a6
David Read <david.read@hackneyworkshop.com>
2013-11-15 21:55:19 +0000

    Added bulk invalidate and exact reconciliation.

--- nomen_entities.py
+++ nomen_entities.py
@@ -1,5 +1,6 @@
 import sys
 import argparse
+import requests
 
 from common import *
 from running_stats import StatsList
@@ -12,14 +13,71 @@ nk_dataset_name = 'uk25k-entities'
 # Alias (unmatched)
 # {'_dataset': <Dataset(uk25k-entities)>, '__data__': {u'created_at': u'2012-09-29T23:02:43.650686', u'name': u'IPS - Identity & Passport Service ', u'creator': {u'updated_at': u'2012-07-18T13:20:27.593385', u'created_at': u'2012-07-18T13:20:27.593374', u'login': u'pudo', u'github_id': 41628, u'id': 1}, u'matcher': None, u'is_invalid': False, u'is_matched': False, u'updated_at': u'2012-09-29T23:02:43.650698', u'entity': None, u'dataset': u'uk25k-entities', u'data': {}, u'id': 25712}}
 
+class NomenData(object):
+    '''Data from Nomenklatura that has been processed and cached.'''
+    def __init__(self):
+        nk_dataset = nk_connect(nk_dataset_name)
+        # Put all the entities and their aliases in entitie_dict, invalid_aliases and unmatched_aliases
+        entities = nk_dataset.entities()
+        self.entities_dict = {} # id: entity_dict
+        self.entities_dict_by_name = {} # name: entity_dict
+        count = 0
+        for entity in entities:
+            count += 1
+            self.entities_dict[entity.id] = {
+                    'entity': entity,
+                    'has_dgu_name': bool(entity.data.get('dgu_name')),
+                    'aliases': []}
+            self.entities_dict_by_name[entity.name] = self.entities_dict[entity.id]
+        self.num_entities = count
+
+        count = 0
+        aliases = nk_dataset.aliases()
+        self.aliases_by_name = {} # name: alias
+        self.invalid_aliases = []
+        self.unmatched_aliases = []
+        for al in aliases:
+            count += 1
+            self.aliases_by_name[al.name] = al
+            if al.is_matched and not al.is_invalid:
+                self.entities_dict[al.entity['id']]['aliases'].append(al)
+                # self.entities_dict_by_name has the same value object, so no
+                # need to update it separately.
+            elif al.is_matched and al.is_invalid:
+                self.invalid_aliases.append(al)
+            else:
+                self.unmatched_aliases.append(al)
+        self.num_aliases = count
+
 def show_all():
-    nk_dataset = nk_connect(nk_dataset_name)
-    entities = nk_dataset.entities()
-    for entity in entities:
-        print entity
-    aliases = nk_dataset.aliases()
-    for al in aliases:
-        print al
+    nomen_data = NomenData()
+    print 'Entities: %i' % nomen_data.num_entities
+    print 'Aliases: %i' % nomen_data.num_aliases
+
+    def title(text):
+        return '\n\n\n%s\n%s\n%s' % ('*' * 70, text, '*' * 70)
+
+    print title('Entity: Alias, Alias, Alias...')
+    def printable_nkobj(alias_or_entity):
+        # Use UTF8 (matching the average shell) so it can be cut/pasted
+        return alias_or_entity.name.encode('utf8', errors='replace')
+    for entity_dict in sorted(nomen_data.entities_dict.values(),
+            key=lambda entity_dict: (entity_dict['has_dgu_name'],
+                                     entity_dict['entity'].name)):
+        entity = entity_dict['entity']
+        aliases = entity_dict['aliases']
+        print printable_nkobj(entity), \
+                ' '.join(['"%s"' % printable_nkobj(alias) for alias in aliases])
+
+    print title('Invalid Aliases')
+    for alias in sorted(nomen_data.invalid_aliases,
+            key=lambda alias: alias.name):
+        print printable_nkobj(alias)
+
+    print title('Unmatched Aliases')
+    for alias in sorted(nomen_data.unmatched_aliases,
+            key=lambda alias: alias.name):
+        print printable_nkobj(alias)
 
 def update_entities_from_dgu(publishers=None):
     dgu_client = ckan_client()
@@ -88,13 +146,116 @@ def update_entities_from_dgu(publishers=None):
     print 'Summary'
     print stats.report()
 
+def reconcile_aliases_that_match_entities_exactly():
+    '''When adding entities using this tool, they might also currently be in
+    the recon queue. In cases there the alias name matches exactly the entity
+    name, link them up.
+
+    (Ideally we'd just delete the alias from the recon queue, but there is no
+    delete_alias API.)
+    '''
+    stats = StatsList()
+    nomen_data = NomenData()
+    nk_dataset = nk_connect(nk_dataset_name)
+    for alias in nomen_data.unmatched_aliases:
+        try:
+            entity_or_alias = nk_dataset.lookup_detailed(alias.name, readonly=True)
+        except (nk_dataset.Invalid, nk_dataset.NoMatch):
+            entity_or_alias = None
+
+        if entity_or_alias and isinstance(entity_or_alias, nomenklatura.Entity):
+            try:
+                nk_dataset.match(alias_id=alias.id, entity_id=entity_or_alias.id)
+            except requests.exceptions.HTTPError, e:
+                # Seem to get occasional 502s due to overloading
+                print stats.add('Server error linking the alias to an entity: %s' % e, alias.name)
+                continue
+            print stats.add('Matched alias to an entity of the same name', alias.name)
+        else:
+            print stats.add('No matching entity', alias.name)
+    print 'Summary'
+    print stats.report()
+
+def bulk_action(action=None, filepath=None, entity_or_alias_names=None, entities=True, aliases=True):
+    nomen_data = NomenData()
+    nk_dataset = nk_connect(nk_dataset_name)
+
+    # Gather the list of entities & aliases from the file and command-line
+    entities_or_aliases = []
+    def find_name(name, stats):
+        if not name.strip():
+            print stats.add('blank', name)
+        elif entities and name in nomen_data.entities_dict_by_name:
+            entities_or_aliases.append(nomen_data.entities_dict_by_name[name]['entity'])
+            print stats.add('Entity found', name)
+        elif aliases and name in nomen_data.aliases_by_name:
+            entities_or_aliases.append(nomen_data.aliases_by_name[name])
+            print stats.add('Alias found', name)
+        else:
+            print stats.add('Not found', name)
+    if entity_or_alias_names:
+        stats = StatsList()
+        for name in entity_or_alias_names:
+            find_name(name, stats)
+        print 'Given names:'
+        print stats.report()
+    if filepath:
+        if not os.path.exists(filepath):
+            raise Exception('Filepath not found: %s' % filepath)
+        with open(filepath, 'r') as f:
+            stats = StatsList()
+            for line in f:
+                name = line.rstrip('\n\r')
+                find_name(name, stats)
+                #try:
+                #    entity_or_alias = nk_dataset.lookup_detailed(publisher['title'], readonly=True)
+                #except nk_dataset.NoMatch:
+                #    print stats.add('Not found', publisher['title'])
+                #    continue
+                #except nk_dataset.Invalid:
+                #    pass
+                #print stats.add('Found %s' % entity_or_alias.__class__.__name__, entity_or_alias.name)
+                #entities_or_aliases.append(entity_or_alias)
+        print 'File names:'
+        print stats.report()
+
+    # Do the action to each entity
+    stats = StatsList()
+    for entity_or_alias in entities_or_aliases:
+        name = entity_or_alias.name
+        if action=='invalidate':
+            if isinstance(entity_or_alias, nomenklatura.Entity):
+                print stats.add('Cannot invalidate an Entity', name)
+                continue
+            alias = entity_or_alias
+            if alias.is_invalid:
+                print stats.add('Already invalid', name)
+                continue
+            try:
+                nk_dataset.match(alias_id=alias.id, entity_id='INVALID')
+            except requests.exceptions.HTTPError, e:
+                # Seem to get occasional 502s due to overloading
+                print stats.add('Server error: %s' % e, alias.name)
+                continue
+            print stats.add('Invalidated', name)
+        else:
+            raise NotImplemented
+    print 'Bulk %s:' % action
+    print stats.report()
+
 if __name__ == '__main__':
     parser1 = argparse.ArgumentParser(description='Manager of Entities in Nomenklatura.')
-    commands = ('show', 'update-entities-from-dgu')
+    commands = (
+        'show',
+        'update-entities-from-dgu',
+        'reconcile-aliases-that-match-entities-exactly',
+        'invalidate',
+        )
     parser1.add_argument('command', choices=commands)
 
     # Split command-line into the command and any args after it
     # (there may be general options before the command)
+    # TODO: use add_subparsers instead
     args1, args2 = sys.argv, []
     args = sys.argv[1:]
     for word_index, word in enumerate(args):
@@ -109,3 +270,14 @@ if __name__ == '__main__':
         parser2.add_argument('publishers', nargs='*', default=None)
         parsed_args2 = parser2.parse_args(args2)
         update_entities_from_dgu(parsed_args2.publishers)
+    elif parsed_args1.command == 'reconcile-aliases-that-match-entities-exactly':
+        reconcile_aliases_that_match_entities_exactly()
+    elif parsed_args1.command == 'invalidate':
+        parser2 = argparse.ArgumentParser()
+        parser2.add_argument('-f', metavar='FILE', dest='filepath')
+        parser2.add_argument('entities', nargs='*', default=None)
+        parsed_args2 = parser2.parse_args(args2)
+        bulk_action(action=parsed_args1.command, filepath=parsed_args2.filepath, entity_or_alias_names=parsed_args2.entities)
+    else:
+        raise NotImplemented
+
